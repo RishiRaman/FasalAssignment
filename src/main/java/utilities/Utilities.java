eee6baf858c2b237a5d53e9d8acd1e657c96a3f4
Rishi hisariya <Rishi.Hisariya@mastercard.com>
2021-09-26 11:34:49 +0530

    Completed Assignment One and Two

--- src/main/java/utilities/Utilities.java
+++ src/main/java/utilities/Utilities.java
@@ -1,11 +1,14 @@
 package utilities;
 
+import com.google.gson.*;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Utilities {
 
@@ -33,5 +36,30 @@ public class Utilities {
         }catch (Exception e){}
         JavascriptExecutor executor = (JavascriptExecutor)driver;
         executor.executeScript("arguments[0].click();", element);
+    }
+
+    public static String formRequestBody(String keyOne, String ValueOne, String keyTwo, String ValueTwo){
+        Map<String, String> requestBodyMap =  new HashMap<String, String>();
+        requestBodyMap.put(keyOne,ValueOne);
+        requestBodyMap.put(keyTwo,ValueTwo);
+        // Convert Map to JSON
+        Gson gson = new Gson();
+        String requestBody = gson.toJson(requestBodyMap);
+
+        return requestBody;
+    }
+
+    public static String singleUserExpectedResponseBody(){
+        String singleUserExpectedResponse = "{\"data\":{\"id\":2,\"email\":\"janet.weaver@reqres.in\",\"first_name\":\"Janet\",\"last_name\":\"Weaver\",\"avatar\":\"https://reqres.in/img/faces/2-image.jpg\"},\"support\":{\"url\":\"https://reqres.in/#support-heading\",\"text\":\"To keep ReqRes free, contributions towards server costs are appreciated!\"}}";
+        JsonObject singleUserExpectedResponseJsonObject = new Gson().fromJson(singleUserExpectedResponse, JsonObject.class);
+
+        return singleUserExpectedResponseJsonObject.toString();
+    }
+
+    public static String listUsersExpectedResponseBody(){
+        String listUserExpectedResponse = "{\"page\":2,\"per_page\":6,\"total\":12,\"total_pages\":2,\"data\":[{\"id\":7,\"email\":\"michael.lawson@reqres.in\",\"first_name\":\"Michael\",\"last_name\":\"Lawson\",\"avatar\":\"https://reqres.in/img/faces/7-image.jpg\"},{\"id\":8,\"email\":\"lindsay.ferguson@reqres.in\",\"first_name\":\"Lindsay\",\"last_name\":\"Ferguson\",\"avatar\":\"https://reqres.in/img/faces/8-image.jpg\"},{\"id\":9,\"email\":\"tobias.funke@reqres.in\",\"first_name\":\"Tobias\",\"last_name\":\"Funke\",\"avatar\":\"https://reqres.in/img/faces/9-image.jpg\"},{\"id\":10,\"email\":\"byron.fields@reqres.in\",\"first_name\":\"Byron\",\"last_name\":\"Fields\",\"avatar\":\"https://reqres.in/img/faces/10-image.jpg\"},{\"id\":11,\"email\":\"george.edwards@reqres.in\",\"first_name\":\"George\",\"last_name\":\"Edwards\",\"avatar\":\"https://reqres.in/img/faces/11-image.jpg\"},{\"id\":12,\"email\":\"rachel.howell@reqres.in\",\"first_name\":\"Rachel\",\"last_name\":\"Howell\",\"avatar\":\"https://reqres.in/img/faces/12-image.jpg\"}],\"support\":{\"url\":\"https://reqres.in/#support-heading\",\"text\":\"To keep ReqRes free, contributions towards server costs are appreciated!\"}}";
+        JsonObject listUserExpectedResponseJsonObject = new Gson().fromJson(listUserExpectedResponse,JsonObject.class);
+
+        return listUserExpectedResponseJsonObject.toString();
     }
 }
---
+++ src/test/java/StepDefinitions/VerifyWebServiceStepDef.java
@@ -0,0 +1,107 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import utilities.Utilities;
+
+public class VerifyWebServiceStepDef {
+
+    private static Response response;
+    private static String baseURI = "https://reqres.in";
+
+    @Given("the POST call is done to REGISTER-SUCCESSFUL Api and response is validated Successfully")
+    public void registerSuccessfulPOSTCall(){
+        RestAssured.baseURI = baseURI + "/api/register";
+        String requestBody = Utilities.formRequestBody("email","eve.holt@reqres.in",
+                "password","pistol");
+
+        response =  RestAssured.given().header("Content-Type","application/json")
+                .body(requestBody).when().post(RestAssured.baseURI);
+
+        Assert.assertEquals(response.statusCode(), 200,
+                "Validate REGISTER-SUCCESSFUL status code is appropriate.");
+        Assert.assertEquals(JsonPath.from(response.getBody().asString()).get("id"),
+                Integer.valueOf("4"),"Validate the id in response body is appropriate");
+        Assert.assertEquals(JsonPath.from(response.getBody().asString()).get("token"),
+                "QpwL5tke4Pnpja7X4","Validate the token in response body is appropriate");
+    }
+
+    @And("the POST call is done to CREATE Api and response is validated Successfully")
+    public void createPOSTCall() {
+        RestAssured.baseURI = baseURI + "/api/users";
+        String requestBody = Utilities.formRequestBody("name", "morpheus",
+                    "job","leader");
+        response =  RestAssured.given().header("Content-Type","application/json")
+                .body(requestBody).when().post(RestAssured.baseURI);
+
+        Assert.assertEquals(response.statusCode(), 201,
+                "Validate CREATE POST call status code is appropriate.");
+        Assert.assertEquals(JsonPath.from(response.getBody().asString()).get("name"),
+                "morpheus","Validate name in response body is appropriate");
+        Assert.assertEquals(JsonPath.from(response.getBody().asString()).get("job"),
+                "leader","Validate the job in response body is appropriate");
+
+    }
+
+    @When("the GET call is done to fetch the SINGE USER and response is validated Successfully")
+    public void singleUserGETCall() {
+        RestAssured.baseURI = baseURI + "/api/users/2";
+        response = RestAssured.given().header("Content-Type","application/json").
+                when().get(RestAssured.baseURI);
+
+        Assert.assertEquals(response.statusCode(), 200,
+                "Validate SINGLE USER GET call status code is appropriate.");
+        Assert.assertEquals(Utilities.singleUserExpectedResponseBody(), response.getBody().asString(),
+                "Validate the SINGLE USER GET call response body is appropriate.");
+
+    }
+
+    @And("the GET call is done to fetch LIST of USERS and response is validated Successfully")
+    public void listUsersGETCall() {
+        RestAssured.baseURI = baseURI + "/api/users";
+        response = RestAssured.given().header("Content-Type","application/json").
+                queryParam("page","2").when().get(RestAssured.baseURI);
+
+        Assert.assertEquals(response.statusCode(), 200,
+                "Validate SINGLE USER GET call status code is appropriate.");
+        Assert.assertEquals(Utilities.listUsersExpectedResponseBody(), response.getBody().asString(),
+                "Validate the SINGLE USER GET call response body is appropriate.");
+    }
+
+    @Then("the PUT call is done to modify user and response is validated Successfully")
+    public void UPDATEUserPUTCall() {
+        RestAssured.baseURI = baseURI + "/api/users/2";
+        String requestBody = Utilities.formRequestBody("name", "morpheus",
+                "job","zion resident");
+
+        response = RestAssured.given().header("Content-Type","application/json").body(requestBody)
+                .when().put(RestAssured.baseURI);
+
+        Assert.assertEquals(response.statusCode(), 200,
+                "Validate SINGLE USER GET call status code is appropriate.");
+        Assert.assertEquals(JsonPath.from(response.getBody().asString()).get("name"),
+                "morpheus","Validate name in response body is appropriate");
+        Assert.assertEquals(JsonPath.from(response.getBody().asString()).get("job"),
+                "zion resident","Validate the job in response body is appropriate");
+    }
+
+    @Then("the DELETE call is done and response is validated Successfully")
+    public void DELETEUserCall() {
+        RestAssured.baseURI = baseURI + "/api/users/2";
+        String requestBody = Utilities.formRequestBody("name", "morpheus",
+                "job","zion resident");
+
+        response = RestAssured.given().header("Content-Type","application/json")
+                .when().delete(RestAssured.baseURI);
+
+        Assert.assertEquals(response.statusCode(), 204,
+                "Validate DELETE user call status code is appropriate.");
+    }
+}
+
